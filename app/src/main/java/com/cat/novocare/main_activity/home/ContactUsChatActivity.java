20073f97bdec53fab12eccf9a0e890a0e938792e
Ma7moudElbaz <dev.mahmoud.elbaz@gmail.com>
2021-05-10 15:33:44 +0200

    Commit 05-05-2021

--- app/src/main/java/com/cat/novocare/main_activity/home/ContactUsChatActivity.java
+++ app/src/main/java/com/cat/novocare/main_activity/home/ContactUsChatActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
