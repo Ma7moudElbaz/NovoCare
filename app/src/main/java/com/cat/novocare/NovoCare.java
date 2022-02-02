3188c610b7565dc46b5e5116cea3c85f5abd7fc2
Ma7moudElbaz <dev.mahmoud.elbaz@gmail.com>
2022-02-02 14:17:59 +0200

    Commit 02-02-2022

--- app/src/main/java/com/cat/novocare/NovoCare.java
+++ app/src/main/java/com/cat/novocare/NovoCare.java
@@ -1,41 +1,14 @@
 package com.cat.novocare;
 
-import android.app.Application;
-import android.content.Context;
-import android.content.res.Configuration;
-
 import androidx.annotation.NonNull;
-
-import com.akexorcist.localizationactivity.core.LocalizationApplicationDelegate;
-
+import com.akexorcist.localizationactivity.ui.LocalizationApplication;
 import java.util.Locale;
 
-import io.customerly.Customerly;
+public class NovoCare extends LocalizationApplication {
 
-public class NovoCare extends Application {
-    final LocalizationApplicationDelegate localizationDelegate = new LocalizationApplicationDelegate();
-
+    @NonNull
     @Override
-    protected void attachBaseContext(Context base) {
-
-        localizationDelegate.setDefaultLanguage(base, Locale.ENGLISH);
-        super.attachBaseContext(localizationDelegate.attachBaseContext(base));
-    }
-
-    @Override
-    public void onConfigurationChanged(@NonNull Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        localizationDelegate.onConfigurationChanged(this);
-    }
-
-    @Override
-    public Context getApplicationContext() {
-        return localizationDelegate.getApplicationContext(super.getApplicationContext());
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Customerly.configure(this, "6284d5e0");
+    public Locale getDefaultLanguage() {
+        return Locale.ENGLISH;
     }
 }
